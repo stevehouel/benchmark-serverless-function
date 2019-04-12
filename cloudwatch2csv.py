94b83b3bb22e36682d09e98437dac11df1bbebb8
HOUEL Steve <shouel@ippon.fr>
2019-04-12 18:07:34 +0200

    Update script for extracting cloudwatch metrics

---
+++ cloudwatch2csv.py
@@ -0,0 +1,49 @@
+import argparse
+import csv
+import datetime as datetime
+import boto3
+import json
+
+# create boto clients
+cw = boto3.client('cloudwatch', region_name='us-east-1')
+lb = boto3.client('lambda', region_name='us-east-1')
+
+
+# extract parameters
+filename = 'output.csv'
+
+# process and write to csv
+with open(filename, 'w') as csvfile:
+    # initialize csv writer
+    csvwriter = csv.writer(
+        csvfile,
+        delimiter=';',
+        quotechar='"',
+        quoting=csv.QUOTE_MINIMAL)
+
+    functions = lb.list_functions(
+        MaxItems=50
+    )
+    initial_time = datetime.datetime(2019, 4, 12, 6, 45, tzinfo=datetime.timezone.utc)
+    for f in functions['Functions']:
+        if 'aws-coldstart' in f['FunctionName']:
+            print(json.dumps(f['FunctionName']))
+            result = cw.get_metric_statistics(
+                MetricName= 'Duration',
+                Namespace= 'AWS/Lambda',
+                Period= 60,
+                Dimensions= [ { 'Name': 'FunctionName', 'Value': f['FunctionName'] } ],
+                Statistics= [ 'Average'],
+                Unit= 'Milliseconds',
+                StartTime=initial_time,
+                EndTime=initial_time + datetime.timedelta(days=1))
+            if 'Datapoints' in result:
+                avg = 0
+                for point in sorted(result['Datapoints'], key=lambda d: d['Timestamp']):
+                    avg += point['Average']
+                csvwriter.writerow([
+                    f['Runtime'] + '-' + str(f['MemorySize']),
+                    avg / len(result['Datapoints'])])
+
+
+    print('CSV file %s created.' % filename)
